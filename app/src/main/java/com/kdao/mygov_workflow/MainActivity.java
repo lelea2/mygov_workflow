50497c15b14aae01181be0de7f29f681850084b1
khanh.dao <khanh.dao@move.com>
2017-04-08 09:10:46 -0700

    Initial check in

--- app/src/main/java/com/kdao/mygov_workflow/MainActivity.java
+++ app/src/main/java/com/kdao/mygov_workflow/MainActivity.java
@@ -1,7 +1,13 @@
 package com.kdao.mygov_workflow;
 
+import android.Manifest;
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupBottomBar();
+    }
+
+    /**
+     * Private function to set up bottom bar
+     */
+    private void setupBottomBar() {
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setDefaultTabPosition(0);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_claim) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                    Intent newIntent = new Intent(MainActivity.this, ClaimActivity.class);
+                    startActivity(newIntent);
+                } else if (tabId == R.id.tab_tracking) {
+                    Intent newIntent = new Intent(MainActivity.this, TrackingActivity.class);
+                    startActivity(newIntent);
+                } else if (tabId == R.id.tab_message) {
+                    Intent newIntent = new Intent(MainActivity.this, MessageActivity.class);
+                    startActivity(newIntent);
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/kdao/mygov_workflow/TrackingActivity.java
+++ app/src/main/java/com/kdao/mygov_workflow/TrackingActivity.java
@@ -1,7 +1,13 @@
 package com.kdao.mygov_workflow;
 
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.content.Intent;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+import android.support.annotation.IdRes;
 
 public class TrackingActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class TrackingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
+        setupBottomBar();
+    }
+
+    /**
+     * Private function to set up bottom bar
+     */
+    private void setupBottomBar() {
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setDefaultTabPosition(2);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_home) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                    Intent newIntent = new Intent(TrackingActivity.this, MainActivity.class);
+                    startActivity(newIntent);
+                } else if (tabId == R.id.tab_claim) {
+                    Intent newIntent = new Intent(TrackingActivity.this, ClaimActivity.class);
+                    startActivity(newIntent);
+                } else if (tabId == R.id.tab_message) {
+                    Intent newIntent = new Intent(TrackingActivity.this, MessageActivity.class);
+                    startActivity(newIntent);
+                }
+            }
+        });
+
     }
 }
